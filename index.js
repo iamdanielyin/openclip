00de50fd59e1d6d8439abc0eec3fabcd46560424
yinfxs <yinfxs@gmail.com>
2018-02-07 23:50:46 +0800

    add files

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 /**
  * 导出模块
  */
-
+const path = require('path');
 const app = require('ibird').newApp({
+  prefix: '/api',
+  uploadDir: __dirname + '/public/upload',
   statics: {
     '/': __dirname + '/public'
   }
-}).play();
+});
+app.post('/upload', async ctx => {
+  const { files } = ctx.request.body;
+  const file = files ? files['file'] : null;
+  if (file) {
+    ctx.body = { data: ctx.origin + `/upload/${path.parse(file.path).base}` };
+  } else {
+    ctx.body = { data: null };
+  }
+})
+app.play();
 
 const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
